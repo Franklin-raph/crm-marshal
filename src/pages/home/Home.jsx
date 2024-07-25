6a13a7ce9528e59b1c6ee913fd8341c42d1c34a0
franklin-raph <igboekwulusifranklin@gmail.com>
2024-07-25 20:50:08 +0100

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,12 +15,12 @@ const Home = () => {
             <Link to='/support' className='text-gray-500'>Support</Link>
         </nav>
         <div className='mt-[5rem] flex items-center flex-col md:flex-row justify-between'>
-            <div className='px-[2rem] lg:px-[5rem] w-full md:text-left text-center md:w-[55%]'>
+            <div className='px-[2rem] lg:px-[5rem] w-full md:text-left text-center lg:w-[60%]'>
                 <h1 className='text-[28px] md:text-[28px] lg:text-[50px] text-gray-700 font-[600]'>Hello, I am CRM Marshal</h1>
                 <p className='mb-6 text-[16px] md:text-[18px] text-gray-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam eum blanditiis earum voluptates natus, id reiciendis distinctio quos vel error.</p>
                 <div className='flex items-center gap-[1rem] justify-center md:justify-start flex-col sm:flex-row'>
-                    <button onClick={() => navigate('/login')} className='bg-[#2B91F3] py-[0.8rem] text-white rounded-full sm:w-[30%] w-[70%] px-[2rem] hover:shadow-lg'>Login</button>
-                    <button onClick={() => navigate('/register')} className='border-2 border-[#2B91F3] py-[0.8rem] text-[#2B91F3] hover:shadow-lg rounded-full sm:w-[30%] w-[70%] px-[2rem]'>Register</button>
+                    <button onClick={() => navigate('/login')} className='bg-[#2B91F3] py-[0.8rem] text-white rounded-full lg:w-[30%] w-[70%] px-[2rem] hover:shadow-lg'>Login</button>
+                    <button onClick={() => navigate('/register')} className='border-2 border-[#2B91F3] py-[0.8rem] text-[#2B91F3] hover:shadow-lg rounded-full lg:w-[30%] w-[70%] px-[2rem]'>Register</button>
                 </div>
             </div>
             <img src="./images/crm1.jpg" alt="" className='md:hidden block w-[90%] rounded-[10px] my-[3rem]'/>
